aa48eb2b72a5e916ab0f1911fe6f60133477dfdf
aaryan200 <105153447+aaryan200@users.noreply.github.com>
2022-12-21 14:18:46 +0530

    Small change

--- ml/imageFaceDetect.py
+++ ml/imageFaceDetect.py
@@ -9,7 +9,7 @@ def image_face_detector(path):
     from mediapipe import solutions
     from faceDetect.settings import MEDIA_ROOT
     from ml.faceDetecModule import FaceDetector
-    resize = False
+    resize_bool = False
     ext = path.split('.')[-1]
     name=(path.split('/')[-1]).split('.')[0]
     detector=FaceDetector(minDetecCon=0.35)
@@ -17,7 +17,7 @@ def image_face_detector(path):
     h,w,c = img.shape
     if (h>1280 and w>720):
         img = resize(img,(1280,720))
-        resize = True
+        resize_bool = True
     img,li=detector.findFaces(img)
     imwrite(join(MEDIA_ROOT,f'img/{name}_result.{ext}'),img)
-    return (resize,len(li))
+    return (resize_bool,len(li))
